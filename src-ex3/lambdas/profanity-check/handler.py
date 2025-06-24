81a38a341ea161b04ca5bcc38f4dbad26650917b
Theresa <92042502+Resi06@users.noreply.github.com>
2025-06-24 16:53:39 +0200

    Update handler.py

--- src-ex3/lambdas/profanity-check/handler.py
+++ src-ex3/lambdas/profanity-check/handler.py
@@ -1,9 +1,78 @@
+import json
+import os
+import typing
+import hashlib
+from urllib.parse import unquote_plus
+import uuid
+import boto3
+from botocore.exceptions import ClientError
+
+from profanity_filter import ProfanityFilter
+
+if typing.TYPE_CHECKING:
+    from mypy_boto3_s3 import S3Client
+    from mypy_boto3_ssm import SSMClient
+    from mypy_boto3_dynamodb import DynamoDBClient, DynamoDBServiceResource
+
+endpoint_url = None
+if os.getenv("STAGE") == "local":
+    endpoint_url = "http://localhost:4566"
+
+args = {
+    "region_name": "us-east-1",
+    "endpoint_url": "http://localhost:4566",
+    "aws_access_key_id": "test",
+    "aws_secret_access_key": "test",
+}
+s3: "S3Client" = boto3.client("s3", **args)
+ssm: "SSMClient" = boto3.client("ssm", **args)
+client: "DynamoDBServiceResource" = boto3.client("dynamodb", **args)
+dynamodb: "DynamoDBClient" = boto3.resource("dynamodb", **args)
+
+pf = ProfanityFilter()
+
+def get_table_name() -> str:
+    parameter = ssm.get_parameter(Name="/localstack-assignment3/dynamodb/profanity")
+    return parameter["Parameter"]["Value"]
+
+
+def get_deterministic_key(data: dict) -> str:
+    reviewer = data.get("reviewerID", "")
+    date = data.get("unixReviewTime", "")
+    asin = data.get("asin", "")
+    identifier = f"{reviewer}+{date}+{asin}"
+    return hashlib.sha256(identifier.encode("utf-8")).hexdigest()
+
+def flag_profanity(review: dict) -> dict:
+    review_text = review.get("reviewText", "")
+    summary = review.get("summary", "")
+
+    contains_profanity = pf.is_profane(review_text) or pf.is_profane(summary)
+
+    return {
+        "id": get_deterministic_key(review),
+        "reviewerID": review.get("reviewerID", ""),
+        "asin": review.get("asin", ""),
+        "reviewText": review_text,
+        "summary": summary,
+        "unixReviewTime": review.get("unixReviewTime", ""),
+        "contains_profanity": contains_profanity
+    }
+
+def write_to_dynamodb(review_item: dict, table_name: str):
+    table = dynamodb.Table(table_name)
+    table.put_item(Item=review_item)
+
 def handler(event, context):
+    table_name = get_table_name()
+
     for record in event['Records']:
-        bucket = record['s3']['bucket']['name']
-        key = record['s3']['object']['key']
+        source_bucket = record['s3']['bucket']['name']
+        key = unquote_plus(record['s3']['object']['key'])
 
-        print(f"New file added: {key} in bucket {bucket}")
+        print(f"Checking profanity and writing to DynamoDB: {key}")
 
-        # Download or read the file content
-        # response = s3.get_object(Bucket=bucket, Key=key)
+        reviews = ReviewSplitter(source_bucket, key)
+        for review in reviews.get():
+            item = flag_profanity(review)
+            write_to_dynamodb(item, table_name)
